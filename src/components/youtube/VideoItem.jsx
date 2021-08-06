e2c28e62d6f8ba346586b49ff45bf4498dd996e0
MGanom <chewy111@naver.com>
2021-08-06 17:16:42 +0900

    add: youtube watch function, youtube close button, design variables in etc tab

--- src/components/youtube/VideoItem.jsx
+++ src/components/youtube/VideoItem.jsx
@@ -4,14 +4,16 @@ import "./styles/VideoItem.scss";
 const VideoItem = ({ video, video: { snippet }, onClick }) => {
   return (
     <li className="videoContainer" onClick={() => onClick(video)}>
-      <img
-        className="thumbnail"
-        src={snippet.thumbnails.medium.url}
-        alt="thumbnail"
-      />
-      <div className="videoInfo">
-        <p className="title">{snippet.title}</p>
-        <p className="channel">{snippet.channelTitle}</p>
+      <div className="videoWrapper">
+        <img
+          className="thumbnail"
+          src={snippet.thumbnails.medium.url}
+          alt="thumbnail"
+        />
+        <div className="videoInfo">
+          <p className="title">{snippet.title}</p>
+          <p className="channel">{snippet.channelTitle}</p>
+        </div>
       </div>
     </li>
   );
